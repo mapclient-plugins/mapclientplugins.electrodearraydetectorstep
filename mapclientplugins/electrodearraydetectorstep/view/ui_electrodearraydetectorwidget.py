723dfa8c1992e7bd097866fcfb48c0914a0768ef
TimmyNic <tim.nicolas@outlook.com>
2021-06-10 14:31:13 +1200

    Updated PySide to PySide2.

--- mapclientplugins/electrodearraydetectorstep/view/ui_electrodearraydetectorwidget.py
+++ mapclientplugins/electrodearraydetectorstep/view/ui_electrodearraydetectorwidget.py
@@ -1,117 +1,169 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mapclientplugins\electrodearraydetectorstep\qt\electrodearraydetectorwidget.ui'
-#
-# Created: Mon Nov 12 14:24:49 2018
-#      by: pyside-uic 0.2.15 running on PySide 1.2.4
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'electrodearraydetectorwidget.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PySide import QtCore, QtGui
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from opencmiss\zincwidgets\basesceneviewerwidget import BaseSceneviewerWidget
+
 
 class Ui_ElectrodeArrayDetectorWidget(object):
     def setupUi(self, ElectrodeArrayDetectorWidget):
-        ElectrodeArrayDetectorWidget.setObjectName("ElectrodeArrayDetectorWidget")
+        if not ElectrodeArrayDetectorWidget.objectName():
+            ElectrodeArrayDetectorWidget.setObjectName(u"ElectrodeArrayDetectorWidget")
         ElectrodeArrayDetectorWidget.resize(870, 576)
-        self.horizontalLayout = QtGui.QHBoxLayout(ElectrodeArrayDetectorWidget)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.controlPanel_groupBox = QtGui.QGroupBox(ElectrodeArrayDetectorWidget)
-        self.controlPanel_groupBox.setObjectName("controlPanel_groupBox")
-        self.verticalLayout = QtGui.QVBoxLayout(self.controlPanel_groupBox)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.time_groupBox = QtGui.QGroupBox(self.controlPanel_groupBox)
-        self.time_groupBox.setObjectName("time_groupBox")
-        self.gridLayout_4 = QtGui.QGridLayout(self.time_groupBox)
-        self.gridLayout_4.setObjectName("gridLayout_4")
-        self.timePlayStop_pushButton = QtGui.QPushButton(self.time_groupBox)
-        self.timePlayStop_pushButton.setObjectName("timePlayStop_pushButton")
+        self.horizontalLayout = QHBoxLayout(ElectrodeArrayDetectorWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.controlPanel_groupBox = QGroupBox(ElectrodeArrayDetectorWidget)
+        self.controlPanel_groupBox.setObjectName(u"controlPanel_groupBox")
+        self.verticalLayout = QVBoxLayout(self.controlPanel_groupBox)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.time_groupBox = QGroupBox(self.controlPanel_groupBox)
+        self.time_groupBox.setObjectName(u"time_groupBox")
+        self.gridLayout_4 = QGridLayout(self.time_groupBox)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.timePlayStop_pushButton = QPushButton(self.time_groupBox)
+        self.timePlayStop_pushButton.setObjectName(u"timePlayStop_pushButton")
+
         self.gridLayout_4.addWidget(self.timePlayStop_pushButton, 1, 1, 1, 1)
-        self.timeValue_label = QtGui.QLabel(self.time_groupBox)
-        self.timeValue_label.setObjectName("timeValue_label")
+
+        self.timeValue_label = QLabel(self.time_groupBox)
+        self.timeValue_label.setObjectName(u"timeValue_label")
+
         self.gridLayout_4.addWidget(self.timeValue_label, 0, 0, 1, 1)
-        self.timeValue_doubleSpinBox = QtGui.QDoubleSpinBox(self.time_groupBox)
-        self.timeValue_doubleSpinBox.setMaximum(12000.0)
-        self.timeValue_doubleSpinBox.setObjectName("timeValue_doubleSpinBox")
+
+        self.timeValue_doubleSpinBox = QDoubleSpinBox(self.time_groupBox)
+        self.timeValue_doubleSpinBox.setObjectName(u"timeValue_doubleSpinBox")
+        self.timeValue_doubleSpinBox.setMaximum(12000.000000000000000)
+
         self.gridLayout_4.addWidget(self.timeValue_doubleSpinBox, 0, 1, 1, 1)
-        self.timeLoop_checkBox = QtGui.QCheckBox(self.time_groupBox)
-        self.timeLoop_checkBox.setObjectName("timeLoop_checkBox")
+
+        self.timeLoop_checkBox = QCheckBox(self.time_groupBox)
+        self.timeLoop_checkBox.setObjectName(u"timeLoop_checkBox")
+
         self.gridLayout_4.addWidget(self.timeLoop_checkBox, 1, 2, 1, 1)
+
+
         self.verticalLayout.addWidget(self.time_groupBox)
-        self.groupBox_2 = QtGui.QGroupBox(self.controlPanel_groupBox)
-        self.groupBox_2.setObjectName("groupBox_2")
-        self.formLayout = QtGui.QFormLayout(self.groupBox_2)
-        self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
-        self.formLayout.setObjectName("formLayout")
-        self.detectElectrodes_pushButton = QtGui.QPushButton(self.groupBox_2)
-        self.detectElectrodes_pushButton.setObjectName("detectElectrodes_pushButton")
-        self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.detectElectrodes_pushButton)
-        self.groupBox_3 = QtGui.QGroupBox(self.groupBox_2)
+
+        self.groupBox_2 = QGroupBox(self.controlPanel_groupBox)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.formLayout = QFormLayout(self.groupBox_2)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setFieldGrowthPolicy(QFormLayout.AllNonFixedFieldsGrow)
+        self.detectElectrodes_pushButton = QPushButton(self.groupBox_2)
+        self.detectElectrodes_pushButton.setObjectName(u"detectElectrodes_pushButton")
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.detectElectrodes_pushButton)
+
+        self.groupBox_3 = QGroupBox(self.groupBox_2)
+        self.groupBox_3.setObjectName(u"groupBox_3")
         self.groupBox_3.setCheckable(True)
         self.groupBox_3.setChecked(False)
-        self.groupBox_3.setObjectName("groupBox_3")
-        self.horizontalLayout_4 = QtGui.QHBoxLayout(self.groupBox_3)
-        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
-        self.trackElectrodePoints_pushButton = QtGui.QPushButton(self.groupBox_3)
-        self.trackElectrodePoints_pushButton.setObjectName("trackElectrodePoints_pushButton")
+        self.horizontalLayout_4 = QHBoxLayout(self.groupBox_3)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.trackElectrodePoints_pushButton = QPushButton(self.groupBox_3)
+        self.trackElectrodePoints_pushButton.setObjectName(u"trackElectrodePoints_pushButton")
+
         self.horizontalLayout_4.addWidget(self.trackElectrodePoints_pushButton)
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout_4.addItem(spacerItem)
-        self.formLayout.setWidget(2, QtGui.QFormLayout.SpanningRole, self.groupBox_3)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_2)
+
+
+        self.formLayout.setWidget(3, QFormLayout.SpanningRole, self.groupBox_3)
+
+        self.reset_pushButton = QPushButton(self.groupBox_2)
+        self.reset_pushButton.setObjectName(u"reset_pushButton")
+
+        self.formLayout.setWidget(5, QFormLayout.LabelRole, self.reset_pushButton)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.formLayout.setItem(4, QFormLayout.LabelRole, self.verticalSpacer_2)
+
+
         self.verticalLayout.addWidget(self.groupBox_2)
-        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem1)
-        self.groupBox = QtGui.QGroupBox(self.controlPanel_groupBox)
-        self.groupBox.setTitle("")
-        self.groupBox.setObjectName("groupBox")
-        self.horizontalLayout_3 = QtGui.QHBoxLayout(self.groupBox)
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.statusText_label = QtGui.QLabel(self.groupBox)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.groupBox = QGroupBox(self.controlPanel_groupBox)
+        self.groupBox.setObjectName(u"groupBox")
+        self.horizontalLayout_3 = QHBoxLayout(self.groupBox)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.statusText_label = QLabel(self.groupBox)
+        self.statusText_label.setObjectName(u"statusText_label")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.statusText_label.sizePolicy().hasHeightForWidth())
         self.statusText_label.setSizePolicy(sizePolicy)
-        self.statusText_label.setObjectName("statusText_label")
+
         self.horizontalLayout_3.addWidget(self.statusText_label)
+
+
         self.verticalLayout.addWidget(self.groupBox)
-        self.frame = QtGui.QFrame(self.controlPanel_groupBox)
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName("frame")
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.frame)
+
+        self.frame = QFrame(self.controlPanel_groupBox)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame)
         self.horizontalLayout_2.setContentsMargins(1, 1, 1, 1)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.done_pushButton = QtGui.QPushButton(self.frame)
-        self.done_pushButton.setObjectName("done_pushButton")
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.done_pushButton = QPushButton(self.frame)
+        self.done_pushButton.setObjectName(u"done_pushButton")
+
         self.horizontalLayout_2.addWidget(self.done_pushButton)
-        spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout_2.addItem(spacerItem2)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+
         self.verticalLayout.addWidget(self.frame)
+
+
         self.horizontalLayout.addWidget(self.controlPanel_groupBox)
+
         self.sceneviewer_widget = BaseSceneviewerWidget(ElectrodeArrayDetectorWidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(0)
+        self.sceneviewer_widget.setObjectName(u"sceneviewer_widget")
         sizePolicy.setHeightForWidth(self.sceneviewer_widget.sizePolicy().hasHeightForWidth())
         self.sceneviewer_widget.setSizePolicy(sizePolicy)
-        self.sceneviewer_widget.setObjectName("sceneviewer_widget")
+
         self.horizontalLayout.addWidget(self.sceneviewer_widget)
 
+
         self.retranslateUi(ElectrodeArrayDetectorWidget)
-        QtCore.QMetaObject.connectSlotsByName(ElectrodeArrayDetectorWidget)
+
+        QMetaObject.connectSlotsByName(ElectrodeArrayDetectorWidget)
+    # setupUi
 
     def retranslateUi(self, ElectrodeArrayDetectorWidget):
-        ElectrodeArrayDetectorWidget.setWindowTitle(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Electrode Array Detector", None, QtGui.QApplication.UnicodeUTF8))
-        self.controlPanel_groupBox.setTitle(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Control Panel", None, QtGui.QApplication.UnicodeUTF8))
-        self.time_groupBox.setTitle(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Time:", None, QtGui.QApplication.UnicodeUTF8))
-        self.timePlayStop_pushButton.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Play", None, QtGui.QApplication.UnicodeUTF8))
-        self.timeValue_label.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Time value:", None, QtGui.QApplication.UnicodeUTF8))
-        self.timeLoop_checkBox.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Loop", None, QtGui.QApplication.UnicodeUTF8))
-        self.groupBox_2.setTitle(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Activity:", None, QtGui.QApplication.UnicodeUTF8))
-        self.detectElectrodes_pushButton.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Detect electrodes", None, QtGui.QApplication.UnicodeUTF8))
-        self.groupBox_3.setTitle(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Track electrode points", None, QtGui.QApplication.UnicodeUTF8))
-        self.trackElectrodePoints_pushButton.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Track", None, QtGui.QApplication.UnicodeUTF8))
-        self.statusText_label.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
-        self.done_pushButton.setText(QtGui.QApplication.translate("ElectrodeArrayDetectorWidget", "Done", None, QtGui.QApplication.UnicodeUTF8))
+        ElectrodeArrayDetectorWidget.setWindowTitle(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Electrode Array Detector", None))
+        self.controlPanel_groupBox.setTitle(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Control Panel", None))
+        self.time_groupBox.setTitle(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Time:", None))
+        self.timePlayStop_pushButton.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Play", None))
+        self.timeValue_label.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Time value:", None))
+        self.timeLoop_checkBox.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Loop", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Activity:", None))
+        self.detectElectrodes_pushButton.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Detect electrodes", None))
+        self.groupBox_3.setTitle(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Track electrode points", None))
+        self.trackElectrodePoints_pushButton.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Track", None))
+        self.reset_pushButton.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Reset", None))
+        self.groupBox.setTitle("")
+        self.statusText_label.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"TextLabel", None))
+        self.done_pushButton.setText(QCoreApplication.translate("ElectrodeArrayDetectorWidget", u"Done", None))
+    # retranslateUi
 
-from opencmiss.zincwidgets.basesceneviewerwidget import BaseSceneviewerWidget
