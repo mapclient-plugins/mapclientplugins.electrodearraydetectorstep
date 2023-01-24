727ffbd0c0e05777d819e28c61f9ce499ea642aa
TimmyNic <tim.nicolas@outlook.com>
2023-01-25 12:09:54 +1300

    Upgrade to PySide6.

--- mapclientplugins/electrodearraydetectorstep/resources_rc.py
+++ mapclientplugins/electrodearraydetectorstep/resources_rc.py
@@ -1,9 +1,9 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 5.15.2
+# Created by: The Resource Compiler for Qt version 6.4.1
 # WARNING! All changes made in this file will be lost!
 
-from PySide2 import QtCore
+from PySide6 import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x0a\xe7\
@@ -210,7 +210,7 @@ qt_resource_struct = b"\
 \x00\x00\x00:\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00L\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01y\xf3\xbaq\xf4\
+\x00\x00\x01\x85\xe6\x06\xcc\xcd\
 "
 
 def qInitResources():
